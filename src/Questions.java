05a5cb2449822a5ac24fbac2fe31b69cf207b9d2
jenishub <118802019+jenishub@users.noreply.github.com>
2022-11-29 14:40:52 +0600

    Add files via upload

--- src/Questions.java
+++ src/Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("Am I the best teacher?: _______");
+        System.out.println("Name: Nurbolat");
+        System.out.println("Surname: Zhenisbayev");
+        System.out.println("Id: 220103275");
+        System.out.println("Hobby: books, games, movies");
+        System.out.println("How much do you like ICT(from 1-10)): 8.5");
+        System.out.println("Rate this task from 1 to 10: 9");
+        System.out.println("Is Yernar the best teacher?: one of the best");
     }
 }
